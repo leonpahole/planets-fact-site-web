a8dfeeffdd723e77f420967b6243fe4727ad2adc
Leon Pahole <leon.pahole@gmail.com>
2023-03-12 13:35:59 +0100

    Add colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,37 @@ const pxToRem = (px) => {
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          700: "#070724",
+          400: "#38384F",
+          100: "#838391",
+        },
+        teal: {
+          100: "#419EBB",
+        },
+        orange: {
+          100: "#EDA249",
+        },
+        purple: {
+          100: "#6D2ED5",
+        },
+        orange: {
+          100: "#CD5120",
+          200: "#D14C32",
+        },
+        red: {
+          100: "#D83A34",
+        },
+        green: {
+          100: "#1EC1A2",
+        },
+        blue: {
+          100: "#2D68F0",
+        },
+      },
+    },
   },
   plugins: [],
 };
