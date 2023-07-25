c3202f5ac80c8b23e04f0848594e66afa1526352
Leon Pahole <leon.pahole@gmail.com>
2023-07-25 20:01:54 +0200

    Move font into document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,11 +8,6 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/favicon-32x32.png" />
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Antonio&family=League+Spartan&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <div className={`font-sans`}>
         <Component {...pageProps} />
