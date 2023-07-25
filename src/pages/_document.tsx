c8cd2aaee8e2aee2ba8b561d0a84a496f96bfec3
Leon Pahole <leon.pahole@gmail.com>
2023-07-25 20:07:52 +0200

    Fix font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Antonio&family=League+Spartan&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
+      <Head />
       <body>
         <Main />
         <NextScript />
